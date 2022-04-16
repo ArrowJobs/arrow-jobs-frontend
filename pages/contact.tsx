ee4e0c8ffed06bbb6d09df13ab98e8134896692c
alphasr <nilaybaranwal@gmail.com>
2022-04-16 20:29:47 +0200

    added skeleton for contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 
 import { Layout } from 'components/Layout';
-import Image from 'next/image';
 import Head from 'next/head';
 import { Box, Button, Card, CardActions, CardContent, CardMedia, Grid, Paper, Typography } from '@mui/material';
-import LiveFromSpace from 'public/live-from-space.jpg';
+import { CardActionArea } from '@mui/material';
 
 const About: React.FC = () => {
   return (
@@ -14,127 +13,59 @@ const About: React.FC = () => {
         <Head>
           <title>Arrowed Jobs</title>
         </Head>
-        <div>
-          <Box sx={{ display: 'flex', justifyContent: 'center', maxWidth: '70%', margin: 'auto', mt: 5 }}>
-            <Box>
-              <Typography variant="h1" sx={{ fontWeight: 'bold', minWidht: '100%', marginRight: 30 }}>
-                Contact&nbsp;Us
-              </Typography>
-            </Box>
-            <Box>
-              <Typography variant="h5" sx={{ letterSpacing: '0.07rem' }}>
-                ArrowedJobs helps companies present themselves to their prospective employees like it has never been
-                done before. We offer a platform for employers that helps prospective hires to experience the company
-                culture remotely.
-              </Typography>
-            </Box>
-          </Box>
-          <Box sx={{ display: 'flex', justifyContent: 'center', maxWidth: '80%', margin: 'auto', mt: 5 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={3} lg={3}>
-                <Paper>
-                  <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center' }}>
-                    <img
-                      src={LiveFromSpace} // Scale your image down to fit into the container
-                    />
-                  </Box>
-
-                  <Card sx={{ minWidht: '100%' }}>
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all
-                        continents except Antarctica
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Share</Button>
-                      <Button size="small">Learn More</Button>
-                    </CardActions>
-                  </Card>
-                </Paper>
-              </Grid>
-              <Grid item xs={3} lg={3}>
-                <Paper>
-                  <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center' }}>
-                    <img
-                      src={LiveFromSpace} // Scale your image down to fit into the container
-                    />
-                  </Box>
-
-                  <Card sx={{ minWidht: '100%' }}>
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all
-                        continents except Antarctica
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Share</Button>
-                      <Button size="small">Learn More</Button>
-                    </CardActions>
-                  </Card>
-                </Paper>
-              </Grid>
-              <Grid item xs={3} lg={3}>
-                <Paper>
-                  <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center' }}>
-                    <img
-                      src={LiveFromSpace} // Scale your image down to fit into the container
-                    />
-                  </Box>
-
-                  <Card sx={{ minWidht: '100%' }}>
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all
-                        continents except Antarctica
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Share</Button>
-                      <Button size="small">Learn More</Button>
-                    </CardActions>
-                  </Card>
-                </Paper>
-              </Grid>
-              <Grid item xs={3} lg={3}>
-                <Paper>
-                  <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center' }}>
-                    <img
-                      src={LiveFromSpace} // Scale your image down to fit into the container
-                    />
-                  </Box>
-
-                  <Card sx={{ minWidht: '100%' }}>
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all
-                        continents except Antarctica
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small">Share</Button>
-                      <Button size="small">Learn More</Button>
-                    </CardActions>
-                  </Card>
-                </Paper>
-              </Grid>
-            </Grid>
-          </Box>
-        </div>
       </div>
+      <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', mx: 'auto' }}>
+        <Card sx={{ maxWidth: 200 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="150"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Report Bugs
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles,
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Email us
+            </Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+        <Card sx={{ display: 'grid', maxWidth: 200 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="150"
+              image="/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Report Bugs
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all
+                continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Email us
+            </Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Box>
     </Layout>
   );
 };
